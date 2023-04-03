b9aeeb803c86fc0ed7d0b8e86ebc94f592d911fa
Husain-Jinia <husainjiniaga@gmail.com>
2023-04-04 00:01:14 +0530

    Added add message func

--- src/components/Chats/message.js
+++ src/components/Chats/message.js
@@ -1,5 +1,6 @@
 import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/authContext";
 import { ChatContext } from "../../context/chatContext";
 import { db } from "../../firebase";
 
@@ -7,11 +8,13 @@ import { db } from "../../firebase";
 const Messages = () => {
 
     const [messages, setMessages] = useState([])
+    const { currentUser } = useContext(AuthContext);
     const {data} = useContext(ChatContext)
 
     useEffect(() => {
       const unsub = onSnapshot(doc(db,"chats",data.chatId), doc=>{
         doc.exists()  && setMessages(doc.data().messages)
+        console.log(doc.data())
       })
     
       return () => {
@@ -25,7 +28,24 @@ const Messages = () => {
             {
                 messages.map(m=>
                     <div key={m.id}>
-                        <p>m.text</p>
+                        {
+                            
+                        <img
+                            style={{ verticalAlign: "middle",
+                            width: "25px",
+                            height: "25px",
+                            marginInline:"10px",
+                            borderRadius: "50%"}}
+                            src={
+                                m.senderId === currentUser.uid
+                                ? currentUser.photoURL
+                                : data.user.photoURL
+                            }
+                            alt=""
+                            />
+                        }
+                         {m.text}
+                        
                     </div>    
                 )
             }
